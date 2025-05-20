b2c5b3b4dd95243ce5e1416409efc0bdb794096b
AdryzR <adrien.raphanaud@epitech.eu>
2025-05-20 11:04:57 +0200

    feat[ui weapons]: handle completely displays based on enum weapon_status

--- src/display/display_main.c
+++ src/display/display_main.c
@@ -114,8 +114,9 @@ int display_main(game_t *game, sfTexture **texture)
         display_shot(game->windows.windows, &game->shot_struct, game);
         sfSound_play(game->shot_struct.shot_sound);
     }
-    sfRenderWindow_drawSprite(game->windows.windows,
-    game->weapon.sprite, NULL);
+    if (game->player->wp_status == W_GUN)
+        sfRenderWindow_drawSprite(game->windows.windows,
+        game->weapon.sprite, NULL);
     sfRenderWindow_display(game->windows.windows);
     return 0;
 }
